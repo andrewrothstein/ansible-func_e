2287ba0bd8194bc01528e10dc7c5194d4728b8e4
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-10-06 09:06:46 -0400

    func-e:0.7.0

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.func_e
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-func_e/actions/workflows/build.yml/badge.svg)
+
+Installs Tetrate Labs [func-e](https://github.com/tetratelabs/func-e/releases).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.func_e
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/tetratelabs/func-e/releases/download
+
+# https://github.com/tetratelabs/func-e/releases/download/v0.7.0/func-e_0.7.0_linux_amd64.tar.gz
+
+dl() {
+    local ver=$1
+    local app=$2
+    local lchecksums=$3
+    local os=$4
+    local arch=$5
+    local archive_type=${6:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="${app}_${ver}_${platform}.${archive_type}"
+    local url="$MIRROR/v$ver/$file"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    local app=$2
+    # https://github.com/tetratelabs/func-e/releases/download/v0.7.0/func-e_0.7.0_checksums.txt
+    local url="$MIRROR/v$ver/${app}_${ver}_checksums.txt"
+    local lchecksums="$DIR/${app}_${ver}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $app $lchecksums darwin arm64
+    dl $ver $app $lchecksums darwin amd64
+    dl $ver $app $lchecksums linux arm64
+    dl $ver $app $lchecksums linux amd64
+    dl $ver $app $lchecksums windows amd64 zip
+}
+
+dl_ver ${1:-0.7.0} func-e
